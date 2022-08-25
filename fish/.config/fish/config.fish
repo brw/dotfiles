ae2d130864884531d084ad6641e99dba0bf1228d
Bas van den Wollenberg <hi@bas.sh>
2022-08-25 16:03:41 +0200

    Use sq instead of v for determining if the command exists or not in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,8 @@
-command -sq brew && source (command -v brew && brew --prefix asdf || echo /opt/asdf-vm)/asdf.fish # source from brew if exists, otherwise use system-installed
+source (command -sq brew && brew --prefix asdf || echo /opt/asdf-vm)/asdf.fish # source from brew if exists, otherwise use system-installed
 command -sq starship && starship init fish | source
 command -sq direnv && direnv hook fish | source
 command -sq zoxide && zoxide init fish | source
 
-set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
 
 bind \er __select_from_last
 bind \e, __commandline_token_search_backward
@@ -11,6 +10,7 @@ bind \e, __commandline_token_search_backward
 if string match -q -- "*microsoft*" (uname -a)
   ulimit -n 65536 # no idea how to set this permanently in WSL
 
+  set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
   if not ss -a | grep -q $SSH_AUTH_SOCK
     rm -f $SSH_AUTH_SOCK
     #setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/Bas/wsl-ssh-agent/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork
