8c6e624a99bade9af4e8549ca80f32e6741951c0
Bas van den Wollenberg <hi@bas.sh>
2022-08-26 13:32:44 +0200

    Fix Neovim keymaps by using vimscript and add brew check in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
+test -e $HOME/.homebrew/bin/brew && eval ($HOME/.homebrew/bin/brew shellenv)
 source (command -sq brew && brew --prefix asdf || echo /opt/asdf-vm)/asdf.fish # source from brew if exists, otherwise use system-installed
 command -sq starship && starship init fish | source
 command -sq direnv && direnv hook fish | source
 command -sq zoxide && zoxide init fish | source
-
 
 bind \er __select_from_last
 bind \e, __commandline_token_search_backward
@@ -13,9 +13,8 @@ if string match -q -- "*microsoft*" (uname -a)
   set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
   if not ss -a | grep -q $SSH_AUTH_SOCK
     rm -f $SSH_AUTH_SOCK
-    #setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/Bas/wsl-ssh-agent/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork
+    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/Bas/wsl-ssh-agent/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",fork
   end
 end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
+test -e $HOME/.iterm2_shell_integration.fish && source $HOME/.iterm2_shell_integration.fish
--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,6 +1,5 @@
 local map = vim.keymap.set
 local silent = { silent = true }
-local expr = { expr = true }
 
 map('n', 'n', "<Cmd>silent execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", silent)
 map('n', 'N', "<Cmd>silent execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", silent)
@@ -15,9 +14,12 @@ map('n', '<C-j>', '<C-w><C-j>')
 map('n', '<C-l>', '<C-w><C-l>')
 map('n', '<C-h>', '<C-w><C-h>')
 
+-- why does this not work :(
 --map('i', '<Esc>', 'pumvisible() ? "<C-e><Esc>" : "<Esc>"', { silent = true, expr = true } )
 --map('i', '<C-c>', 'pumvisible() ? "<C-e><C-c>" : "<C-c>"', { silent = true, expr = true } )
 --map('i', '<BS>', 'pumvisible() ? "<C-e><BS>" : "<BS>"', { silent = true, expr = true } )
-map('i', '<BS>', '\\<lt>BS>', { expr = true } )
-
 --map('i', '<CR>', 'pumvisible() ? (complete_info().selected == -1 ? "<C-e><CR>" : "<C-y>") : "<CR>', { silent = true, expr = true } )
+vim.cmd('ino <silent><expr> <Esc>   pumvisible() ? "\\<C-e><Esc>" : "\\<Esc>"')
+vim.cmd('ino <silent><expr> <C-c>   pumvisible() ? "\\<C-e><C-c>" : "\\<C-c>"')
+vim.cmd('ino <silent><expr> <BS>    pumvisible() ? "\\<C-e><BS>"  : "\\<BS>"')
+vim.cmd('ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\\<C-e><CR>" : "\\<C-y>") : "\\<CR>"')
