ac03e73ef68e353e8a4b769d8655eb75800270e8
Bas van den Wollenberg <hi@bas.sh>
2022-09-10 02:33:07 +0200

    Update autocmds

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -4,10 +4,10 @@ vim.cmd('au VimLeave * set guicursor=a:block-blinkon1')
 -- vim.api.nvim_create_autocmd('VimLeave', { command = 'set guicursor=a:block-blinkon1' })
 
 -- press q to close help
--- vim.cmd('au FileType help nmap <silent> <buffer> q :q<CR>')
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'help',
   command = 'nmap <silent> <buffer> q :q<CR>',
 })
 
+-- prevent fidget from throwing a fit if you exit it while it's doing shit
 vim.api.nvim_create_autocmd('VimLeavePre', { command = [[silent! FidgetClose]] })
