e2481c9a64b3dc150bc4e844ce136e31e6f8a81b
Bas van den Wollenberg <hi@bas.sh>
2022-11-29 03:53:53 +0100

    autocmd no longer necessary after fidget update

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -3,6 +3,3 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'help',
   command = 'nmap <silent> <buffer> q :q<CR>',
 })
-
--- prevent fidget from throwing a fit if you exit it while it's doing shit
-vim.api.nvim_create_autocmd('VimLeavePre', { command = [[silent! FidgetClose]] })
