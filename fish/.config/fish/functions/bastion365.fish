e338ceec0c744822e60f0760fff1f5f0b81ca1fd
Bas van den Wollenberg <hi@bas.sh>
2023-03-02 10:56:28 +0100

    Add work Docker compose file environment variable and command

---
+++ fish/.config/fish/functions/bastion365.fish
@@ -0,0 +1,3 @@
+function bastion365 --wraps='docker-compose -f /home/bas/dev/DBX.WebApp/docker-compose.yml' --description 'alias bastion365 docker-compose -f /home/bas/dev/DBX.WebApp/docker-compose.yml'
+  docker-compose -f /home/bas/dev/DBX.WebApp/docker-compose.yml $argv; 
+end
