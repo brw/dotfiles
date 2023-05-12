f3c86ab7d138eecee2e6ff4dc755fdd3fad486aa
Bas van den Wollenberg <hi@bas.sh>
2023-05-12 20:47:48 +0200

    Remove asdf and set HOMEBREW_TEMP for Codam iMacs to be external SSD

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,9 @@ if string match -q -- "*microsoft*" (uname -a)
   ulimit -n 65535 # no idea how to set this permanently in WSL
 
   set -gx DBUS_SESSION_BUS_ADDRESS unix:path=/run/user/1000/bus
-
   set -gx DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
-
   set -gx SSH_AUTH_SOCK $HOME/.ssh/agent.sock
+
   if not ss -a | grep -q $SSH_AUTH_SOCK
     rm -f $SSH_AUTH_SOCK
 
@@ -23,8 +22,7 @@ else if string match -q -- "*codam.nl*" $host
   ulimit -n 10240
 
   #set -gx HOMEBREW_CORE_GIT_REMOTE "https://github.com/gromgit/homebrew-core-mojave"
-
-  set -gx ASDF_DATA_DIR /Volumes/T7/asdf
+  set -gx HOMEBREW_TEMP /Volumes/T7/homebrew/tmp
 
   fish_add_path -g /Applications/CLion.app/Contents/bin/gdb/mac/bin
 else if string match -iq -- "*mistergreen*" $host
@@ -39,12 +37,6 @@ else if test -e $HOME/.homebrew/bin/brew
   eval ($HOME/.homebrew/bin/brew shellenv)
 end
 
-if command -sq brew && test -d (brew --prefix asdf)
-  source (brew --prefix asdf)/libexec/asdf.fish
-else if test -d /opt/asdf-vm
-  source /opt/asdf-vm/asdf.fish
-end
-
 command -sq rtx && rtx activate fish | source
 command -sq starship && starship init fish | source
 command -sq direnv && direnv hook fish | source
@@ -56,5 +48,4 @@ bind \e, __commandline_token_search_backward
 test -e $HOME/.iterm2_shell_integration.fish && source $HOME/.iterm2_shell_integration.fish
 
 set -gx plug_path $HOME/.local/share/fish/plug
-
 set -gx GPG_TTY (tty)
