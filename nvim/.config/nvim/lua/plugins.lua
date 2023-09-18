c55f2b6c0d33c66963256b4962725eb302f4466b
Bas van den Wollenberg <hi@bas.sh>
2023-09-19 00:56:24 +0200

    Add Neotree

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -285,6 +285,16 @@ require("lazy").setup({
   },
 
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "muniftanjim/nui.nvim",
+    },
+  },
+
+  {
     "folke/noice.nvim",
     dependencies = {
       "muniftanjim/nui.nvim",
