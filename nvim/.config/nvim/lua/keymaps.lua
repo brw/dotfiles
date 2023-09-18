d13faa70b41d45bf987e1dbb277dd33a432faa4d
Bas van den Wollenberg <hi@bas.sh>
2023-09-19 00:56:33 +0200

    Only dismiss notifications if no ongoing search

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,24 +9,27 @@ local silent = { silent = true }
 -- map('n', 'g#', "g#<Cmd>lua require('hlslens').start()<CR>", silent)
 -- map('n', '<Esc>', '<Cmd>noh<CR>', silent)
 
-map('n', '<C-k>', '<C-w><C-k>')
-map('n', '<C-j>', '<C-w><C-j>')
-map('n', '<C-l>', '<C-w><C-l>')
-map('n', '<C-h>', '<C-w><C-h>')
+map("n", "<C-k>", "<C-w><C-k>")
+map("n", "<C-j>", "<C-w><C-j>")
+map("n", "<C-l>", "<C-w><C-l>")
+map("n", "<C-h>", "<C-w><C-h>")
 
 -- why does this not work :(
 --map('i', '<Esc>', 'pumvisible() ? "<C-e><Esc>" : "<Esc>"', { silent = true, expr = true } )
 --map('i', '<C-c>', 'pumvisible() ? "<C-e><C-c>" : "<C-c>"', { silent = true, expr = true } )
 --map('i', '<BS>', 'pumvisible() ? "<C-e><BS>" : "<BS>"', { silent = true, expr = true } )
 --map('i', '<CR>', 'pumvisible() ? (complete_info().selected == -1 ? "<C-e><CR>" : "<C-y>") : "<CR>', { silent = true, expr = true } )
-vim.cmd('ino <silent><expr> <Esc>   pumvisible() ? "\\<C-e><Esc>" : "\\<Esc>"')
-vim.cmd('ino <silent><expr> <C-c>   pumvisible() ? "\\<C-e><C-c>" : "\\<C-c>"')
-vim.cmd('ino <silent><expr> <BS>    pumvisible() ? "\\<C-e><BS>"  : "\\<BS>"')
-vim.cmd('ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\\<C-e><CR>" : "\\<C-y>") : "\\<CR>"')
+vim.cmd('ino <silent><expr> <Esc>pumvisible() ? "\\<C-e><Esc>" : "\\<Esc>"')
+vim.cmd('ino <silent><expr> <C-c>pumvisible() ? "\\<C-e><C-c>" : "\\<C-c>"')
+vim.cmd('ino <silent><expr> <BS>pumvisible() ? "\\<C-e><BS>"  : "\\<BS>"')
+vim.cmd('ino <silent><expr> <CR>pumvisible() ? (complete_info().selected == -1 ? "\\<C-e><CR>" : "\\<C-y>") : "\\<CR>"')
 
 --map('n', '<leader>s', '<cmd>SwapSplit<CR>')
 
-map('n', '<Esc>', function()
-  vim.cmd.nohlsearch()
-  require('noice').cmd('dismiss')
+map("n", "<Esc>", function()
+  if vim.v.hlsearch == 1 then
+    vim.cmd.nohlsearch()
+  else
+    require("noice").cmd("dismiss")
+  end
 end, silent)
