33966a0e9ad06de65fcf39417d47c8ebabba7de8
Bas van den Wollenberg <hi@bas.sh>
2023-09-27 02:46:03 +0200

    Treat .envrc as bash

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,5 +1,10 @@
 -- press q to close help
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'help',
-  command = 'nmap <silent> <buffer> q :q<CR>',
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "nmap <silent> <buffer> q :q<CR>",
+})
+
+vim.api.nvim_create_autocmd("BufRead,BufNewFile", {
+  pattern = ".envrc",
+  command = "set filetype=bash",
 })
