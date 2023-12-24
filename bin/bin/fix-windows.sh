454b6d6807d7ca96dcb24f75baff722f56e0f808
Bas van den Wollenberg <hi@bas.sh>
2023-12-24 04:11:43 +0100

    Change monitor IDs

--- bin/bin/fix-windows.sh
+++ bin/bin/fix-windows.sh
@@ -2,4 +2,4 @@
 # Turn my second monitor off and on to trigger Gnome repositioning all windows.
 # Workaround for a Gnome Wayland bug where windows are stuck being maximized
 # while actually only take up part of the screen after the screen wakes up from sleep.
-gnome-randr --output DP-1 --off && gnome-randr --output DP-1 --auto
+gnome-randr --output DP-3 --off && gnome-randr --output DP-3 --auto
