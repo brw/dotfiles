716659ba043309dcc3c31c73c17846b026fe365a
Bas van den Wollenberg <hi@bas.sh>
2023-12-29 01:32:03 +0100

    Automatically open and close dap-ui when debugging

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -628,5 +628,20 @@ require("lazy").setup({
         end,
       },
     },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
   },
 })
