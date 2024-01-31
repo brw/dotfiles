4d4be629942e0bffebcb5dbca9cdf2a41739bf9f
Bas van den Wollenberg <hi@bas.sh>
2024-01-31 21:55:06 +0100

    Create augroup

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,10 +1,15 @@
+local group = vim.api.nvim_create_augroup("yeet", { clear = true })
+
 -- press q to close help
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "help",
   command = "nmap <silent> <buffer> q :q<CR>",
+  group = group,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = ".envrc*",
   command = "set filetype=bash",
+  group = group,
+})
 })
