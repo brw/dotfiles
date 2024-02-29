1873d2d4b6438ed97c1b5437251d2da9dd4fb9e3
Bas van den Wollenberg <hi@bas.sh>
2024-02-29 05:36:44 +0100

    Switch from efm to conform.nvim
    lol

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -23,18 +23,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
   group = group,
 })
-
-local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-
-vim.api.nvim_create_autocmd('BufWritePost', {
-  callback = function(ev)
-    local efm = vim.lsp.get_active_clients({ name = 'efm', bufnr = ev.buf })
-
-    if vim.tbl_isempty(efm) then
-      return
-    end
-
-    vim.lsp.buf.format({ name = 'efm' })
-  end,
-  group = lsp_fmt_group,
-})
