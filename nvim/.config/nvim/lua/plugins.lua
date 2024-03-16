3700d456c00ac3dc6af140a4b7faa19072ac3a9f
Bas van den Wollenberg <hi@bas.sh>
2024-03-16 21:24:37 +0100

    Add dropbar.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -719,4 +719,17 @@ require("lazy").setup({
       },
     },
   },
+
+  {
+    "Bekaboo/dropbar.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    config = function()
+      require("dropbar").setup()
+
+      vim.cmd.highlight("WinBar guibg=none")
+      vim.cmd.highlight("WinBarNC guibg=none")
+    end,
+  },
 })
