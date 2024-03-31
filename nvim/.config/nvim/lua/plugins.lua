f230fc6a9fc7b8d15203b36ac14d04a64d1e2d8c
Bas van den Wollenberg <hi@bas.sh>
2024-03-31 20:23:07 +0200

    Add nvim-nio dependency for nvim-dap

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -652,6 +652,7 @@ require("lazy").setup({
     "mfussenegger/nvim-dap",
     dependencies = {
       "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
       "theHamsta/nvim-dap-virtual-text",
       {
         "ldelossa/nvim-dap-projects",
