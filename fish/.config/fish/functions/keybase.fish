63967579189cc4c279676eb7fb179a13f420c5b6
Bas van den Wollenberg <hi@bas.sh>
2024-08-07 22:00:39 +0200

    Add keybase on Linux and set Wayland environment variables

--- fish/.config/fish/functions/keybase.fish
+++ fish/.config/fish/functions/keybase.fish
@@ -1,3 +1,9 @@
 function keybase --wraps=keybase.exe --description 'alias keybase keybase.exe'
-    keybase.exe $argv
+    if command -v keybase >/dev/null
+        command keybase $argv
+    else if command -v keybase.exe >/dev/null
+        keybase.exe $argv
+    else
+        return 1
+    end
 end
