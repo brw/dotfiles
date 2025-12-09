776a74b85a6e855793bd5a56c66f134a4aa788eb
Bas van den Wollenberg <hi@bas.sh>
2025-12-09 19:08:13 +0100

    only use pacman config if it exists

--- fish/.config/fish/functions/pacman.fish
+++ fish/.config/fish/functions/pacman.fish
@@ -1,3 +1,7 @@
 function pacman
-    command pacman --config /home/bas/.config/pacman/pacman.conf $argv
+    if test -f /home/bas/.config/pacman/pacman.conf
+        command pacman --config /home/bas/.config/pacman/pacman.conf $argv
+    else
+        command pacman $argv
+    end
 end
