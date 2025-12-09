d1455790b509dd46a254a7996ead745d89f73921
Bas van den Wollenberg <hi@bas.sh>
2025-12-09 19:07:44 +0100

    disable man colored less alias in favor of nvim as MANPAGER

--- fish/.config/fish/functions/man.fish
+++ fish/.config/fish/functions/man.fish
@@ -1,7 +1,8 @@
-function man --wraps man --description 'Format and display manual pages'
-    set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o red)
+# disabled in favor of using nvim as MANPAGER
+function _man --wraps man --description 'Format and display manual pages'
+    set -q man_blink; and set -l blink (set_color $man_blink); or set -l blink (set_color -o ff0000)
     set -q man_bold; and set -l bold (set_color $man_bold); or set -l bold (set_color -o 5fafd7)
-    set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color 949494)
+    set -q man_standout; and set -l standout (set_color $man_standout); or set -l standout (set_color 2e3440 -b 88c0d0)
     set -q man_underline; and set -l underline (set_color $man_underline); or set -l underline (set_color -u afafd7)
 
     set -l end (printf "\e[0m")
