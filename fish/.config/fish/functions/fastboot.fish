e1a8f2ca8b6f8b5f3f684c2cc491f88f33116665
Bas van den Wollenberg <hi@bas.sh>
2025-12-09 18:02:19 +0100

    update fastboot command

--- fish/.config/fish/functions/fastboot.fish
+++ fish/.config/fish/functions/fastboot.fish
@@ -1,3 +1,9 @@
 function fastboot --wraps=fastboot.exe --description 'alias fastboot=fastboot.exe'
-    fastboot.exe $argv
+    if command -v fastboot >/dev/null
+        command fastboot $argv
+    else if command -v fastboot.exe >/dev/null
+        fastboot.exe $argv
+    else
+        echo "fastboot not found"
+    end
 end
